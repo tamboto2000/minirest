dc6b7f3005ba0a70d2881f27e8a4c473098194b6
Franklin Collin Tamboto <tamboto2000@gmail.com>
2020-11-03 01:10:16 +0700

    Move Endpoints to separate file

--- controller.go
+++ controller.go
@@ -24,53 +24,6 @@ type Controller interface {
 	Endpoints() *Endpoints
 }
 
-type endpoint struct {
-	method   string
-	path     string
-	callback interface{}
-}
-
-//Endpoints register handlers its path and method
-type Endpoints struct {
-	basePath  string
-	endpoints []endpoint
-}
-
-//BasePath set base path for endpoints
-func (ep *Endpoints) BasePath(path string) {
-	ep.basePath = path
-}
-
-//Add add endpoint with custom method
-func (ep *Endpoints) Add(method, path string, callback interface{}) {
-	ep.endpoints = append(ep.endpoints, endpoint{method, path, callback})
-}
-
-//GET add endpoint with method GET
-func (ep *Endpoints) GET(path string, callback interface{}) {
-	ep.endpoints = append(ep.endpoints, endpoint{"GET", path, callback})
-}
-
-//DELETE add endpoint with method DELETE
-func (ep *Endpoints) DELETE(path string, callback interface{}) {
-	ep.endpoints = append(ep.endpoints, endpoint{"DELETE", path, callback})
-}
-
-//POST add method endpoint with method POST
-func (ep *Endpoints) POST(path string, callback interface{}) {
-	ep.endpoints = append(ep.endpoints, endpoint{"POST", path, callback})
-}
-
-//PUT add method endpoint with method PUT
-func (ep *Endpoints) PUT(path string, callback interface{}) {
-	ep.endpoints = append(ep.endpoints, endpoint{"PUT", path, callback})
-}
-
-//PATCH add method endpoint with method PATCH
-func (ep *Endpoints) PATCH(path string, callback interface{}) {
-	ep.endpoints = append(ep.endpoints, endpoint{"PATCH", path, callback})
-}
-
 //wrapper for request without body, such as GET and DELETE
 func handleWithoutBody(callback interface{}) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, pathVars httprouter.Params) {
